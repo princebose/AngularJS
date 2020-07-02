5b0f87341f6a9dc8fb812b6777b5b3f4c0386576
princebose <boseprince11@gmail.com>
2020-07-02 15:44:03 -0400

    xyz

--- Demo/src/app/test/test.component.ts
+++ Demo/src/app/test/test.component.ts
@@ -4,13 +4,15 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `<h2>Hello World {{name}}</h2>
   <h3>{{greetUser(name)}}</h3>
-  <h3>{{siteUrl}}</h3>`,
+  <h3>{{siteUrl}}</h3>
+  <input id = {{myId}} type="text" value="Bangalore">`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   public siteUrl = window.location.href;
   public name = "John"
+  public myId = "TestID"
   constructor() { }
 
   ngOnInit(): void {
